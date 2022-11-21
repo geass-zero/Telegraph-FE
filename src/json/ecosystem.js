3cbadaefb2990cd1e7dc364e86c6f814ae3b462d
Ribin Roy <ribinroy4@gmail.com>
2022-11-22 00:52:51 +0530

    ecosystem done

--- src/json/ecosystem.js
+++ src/json/ecosystem.js
@@ -33,54 +33,6 @@ const dummyData = [
     icon: image,
     title: 'Avalanche',
   },
-  {
-    icon: image,
-    title: 'HECO Chain',
-  },
-  {
-    icon: image,
-    title: 'Concordium',
-  },
-  {
-    icon: image,
-    title: 'Aptos',
-  },
-  {
-    icon: image,
-    title: 'Solana',
-  },
-  {
-    icon: image,
-    title: 'Matic',
-  },
-  {
-    icon: image,
-    title: 'Avalanche',
-  },
-  {
-    icon: image,
-    title: 'HECO Chain',
-  },
-  {
-    icon: image,
-    title: 'Concordium',
-  },
-  {
-    icon: image,
-    title: 'Aptos',
-  },
-  {
-    icon: image,
-    title: 'Solana',
-  },
-  {
-    icon: image,
-    title: 'Matic',
-  },
-  {
-    icon: image,
-    title: 'Avalanche',
-  },
 ];
 
 export default dummyData;
